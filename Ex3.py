665c6e89ac02b0944b1794eb75b7a91065feaf17
Elena Bochkova <planeado@gmail.com>
2023-11-10 14:40:04 +0300

    Third task for week 2

---
+++ Ex3.py
@@ -0,0 +1,36 @@
+import requests
+
+response_without_method = requests.get('https://playground.learnqa.ru/ajax/api/compare_query_type')
+print(response_without_method.text)
+#Результат: Wrong method provided
+
+payload = {"method": "HEAD"}
+response_head = requests.get('https://playground.learnqa.ru/ajax/api/compare_query_type',
+                                       params=payload)
+print(response_head.text)
+#Результат: Wrong method provided
+
+payload = {"method": "GET"}
+response_get = requests.get('https://playground.learnqa.ru/ajax/api/compare_query_type',
+                                       params=payload)
+print(response_get.text)
+#Результат: {"success":"!"}
+
+combination = [{"method": "GET"}, {"method": "POST"}, {"method": "PUT"}, {"method": "DELETE"}]
+
+for i in range(len(combination)):
+    response_get = requests.get('https://playground.learnqa.ru/ajax/api/compare_query_type',
+                                       params=combination[i])
+    response_post = requests.post('https://playground.learnqa.ru/ajax/api/compare_query_type',
+                                       data=combination[i])
+    response_put = requests.put('https://playground.learnqa.ru/ajax/api/compare_query_type',
+                                  data=combination[i])
+    response_delete = requests.delete('https://playground.learnqa.ru/ajax/api/compare_query_type',
+                                  data=combination[i])
+    print(f"get with {combination[i]['method']} = {response_get.text}")
+    print(f"post with {combination[i]['method']} = {response_post.text}")
+    print(f"put with {combination[i]['method']} = {response_put.text}")
+    print(f"delete with {combination[i]['method']} = {response_delete.text}")
+#Неверный ответ только один: delete with GET = {"success":"!"}
+
+
