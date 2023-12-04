2e14f078851df7bbd6ff390bb956619a01f7c5e4
Elena Bochkova <planeado@gmail.com>
2023-12-04 16:07:09 +0300

    week 4 hometask 3

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -2,6 +2,7 @@ from ..lib.my_requests import MyRequests
 from ..lib.base_case import BaseCase
 from ..lib.assertions import Assertions
 
+
 class TestUserEdit(BaseCase):
     def test_edit_just_created_user(self):
         #REGISTER
@@ -51,3 +52,70 @@ class TestUserEdit(BaseCase):
             new_name,
             "Wrong name of the user after edit"
         )
+
+    def test_edit_user_being_not_authorized(self):
+        # EDIT
+        new_name = "Changed Name"
+
+        response = MyRequests.put(
+            f"/user/2",
+            data={"firstName": new_name}
+        )
+
+        Assertions.assert_code_status(response, 400)
+        Assertions.assert_text_response_is(
+            response,
+            "Auth token not supplied"
+        )
+
+    def test_edit_another_user(self):
+        #REGISTER
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data['email']
+        password = register_data['password']
+        user_id = self.get_json_value(response1, "id")
+
+        #REGISTER2
+        register_data2 = self.prepare_registration_data()
+        response2 = MyRequests.post("/user/", data=register_data2)
+
+        Assertions.assert_code_status(response2, 200)
+        Assertions.assert_json_has_key(response2, "id")
+
+        user_id2 = self.get_json_value(response2, "id")
+
+        # LOGIN
+        login_data = {
+            'email': register_data2['email'],
+            'password': register_data2['password']
+        }
+        response3 = MyRequests.post("/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response3, "auth_sid")
+        token = self.get_header(response3, "x-csrf-token")
+
+        # EDIT
+        new_name = "Changed Name"
+
+        response4 = MyRequests.put(
+            f"/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"firstName": new_name}
+        )
+
+        print(response4.text)
+        print(response4.status_code)
+        print(user_id)
+        print(user_id2)
+
+       # Assertions.assert_code_status(response2, 400)
+       # Assertions.assert_text_response_is(
+       #     response2,
+       #     "Please, do not edit test users with ID 1, 2, 3, 4 or 5"
+       # )
